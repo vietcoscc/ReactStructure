746a1985cf35069c4a9dd252cdbceb1364a80bec
Ominext <vietnq@ominext.com>
2018-07-13 08:25:22 +0700

    tmp commit

--- app/components/Header/styles.js
+++ app/components/Header/styles.js
@@ -1,19 +1,20 @@
 import { StyleSheet } from 'react-native'
 import { screenWidth, screenHeight } from './../../constants/dimensions'
+const heightRatio = 12;
 export default StyleSheet.create({
     containers: {
         width: screenWidth,
         backgroundColor: '#1976D2',
-        height: screenHeight / 11
+        height: screenHeight / heightRatio
     },
     drawerButton: {
-        width: screenHeight / 11,
-        height: screenHeight / 11,
+        width: screenHeight / heightRatio,
+        height: screenHeight / heightRatio,
         alignItems: 'center',
         justifyContent: 'center'
     },
-    drawerButtonIcon:{
-        width: screenHeight/11*1/2,
-        height: screenHeight/11*1/2,
+    drawerButtonIcon: {
+        width: screenHeight / heightRatio * 1 / 2,
+        height: screenHeight / heightRatio * 1 / 2,
     }
 })
